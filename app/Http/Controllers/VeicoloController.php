aef46807ac40db6c8bc0df5601ed045c63be5f5c
Raphael Leonelli <lurgouf82@hotmail.com>
2023-08-22 11:04:57 +0200

    init all models and controllers

--- app/Http/Controllers/VeicoloController.php
+++ app/Http/Controllers/VeicoloController.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use App\Models\Societa;
+use App\Models\Veicolo;
 
 
 
